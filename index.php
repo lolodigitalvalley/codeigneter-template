035f1d6eb0b0f5bab7c5a2b0dec3308daabd58b5
lolodigitalvalley <grand1010.mail@gmail.com>
2017-03-29 12:55:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,11 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+if (function_exists('date_default_timezone_set')) {
+	date_default_timezone_set('Asia/Jakarta');
+}
+	
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
  *---------------------------------------------------------------
